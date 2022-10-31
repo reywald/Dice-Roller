022c154f63a7ced535022345d8a824e83f45ef38
Ikechukwu Agbarakwe <iaweepingsaint@gmail.com>
2022-10-31 18:37:37 +0100

    Add another dice to the app. Clicking on the Roll button should roll 2 dice. The results should be displayed in 2 different TextViews on screen.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -21,10 +21,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun rollDice() {
-        val dice = Dice(6)
-        val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val dice1 = Dice(6)
+        val dice2 = Dice(6)
+        val diceRoll1 = dice1.roll()
+        val diceRoll2 = dice2.roll()
+        val resultTextView1: TextView = findViewById(R.id.textView)
+        resultTextView1.text = diceRoll1.toString()
+        val resultTextView2: TextView = findViewById(R.id.textView2)
+        resultTextView2.text = diceRoll2.toString()
     }
 }
 
